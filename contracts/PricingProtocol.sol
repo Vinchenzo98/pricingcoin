4305969efe1d753adaa5c5dbe98aef2486d61dcf
alangindi <agindi@usc.edu>
2021-07-15 10:06:49 +0200

    Update PricingProtocol.sol

--- contracts/PricingProtocol.sol
+++ contracts/PricingProtocol.sol
@@ -6,14 +6,16 @@ contract PricingCoin {
     address manager;
     uint startTime;
     uint endTime;
+    uint totalAppraisalValue;
+    uint finalAppraisalPrice;
     
     constructor() public {
         manager = msg.sender;
         startTime = now;
-        endTime = now + 1 days; 
+        endTime = now + 1 days;
     }
     
-    mapping(address => uint) private finalAppraisalPrice;
+    // mapping(address => uint) private finalAppraisalPrice;
     
     struct Voter {
         uint appraisal;
@@ -22,7 +24,7 @@ contract PricingCoin {
     }
     
     mapping (address => Voter) voters;
-    address[] public addresses;
+    address[] addresses;
     
     //Check if contract is active
     modifier isActive {
@@ -45,6 +47,7 @@ contract PricingCoin {
     //Allow users to create new vote.
     function setVote(uint _appraisal) checkStake isActive oneVoteEach payable public {
         Voter memory newVote = Voter(_appraisal, msg.value, true);
+        totalAppraisalValue += _appraisal; 
         voters[msg.sender] = newVote;
         addresses.push(msg.sender);
     }
@@ -57,8 +60,21 @@ contract PricingCoin {
         return voters[msg.sender].appraisal;
     }
     
+    function setStake(address a, uint _num) public returns(bool) {
+        voters[a].stake = _num;
+        return true; 
+    }
+    
     function getStake() view public returns(uint) {
         return voters[msg.sender].stake;
+    }
+    
+    function setFinalAppraisal() public returns(uint) {
+        finalAppraisalPrice = totalAppraisalValue/addresses.length;
+    }
+    
+    function getFinalAppraisal() view public returns(uint) {
+        return finalAppraisalPrice;
     }
     
     /*
@@ -81,10 +97,21 @@ contract PricingCoin {
     
     Should return amount total loss harvest amount 
     */
-    function harvestLoss() internal returns(uint){
-        
-    }
-    
+   function harvestLoss(address a) view public returns(uint){
+        if (voters[a].appraisal*1000 > 21000*finalAppraisalPrice/20){
+            voters[a].stake = 
+                (voters[a].stake - (voters[a].appraisal*1000 - 21000*finalAppraisalPrice/20)
+                /finalAppraisalPrice*voters[a].stake);
+        }
+        else if(voters[a].appraisal < 19*finalAppraisalPrice/20){
+            voters[a].stake = 
+                (voters[a].stake - (19*finalAppraisalPrice/20 - voters[a].appraisal)
+                /finalAppraisalPrice*voters[a].stake);
+        }
+        else {
+            return 0;
+        }
+    }    
     /*
     Loss pool should be divided by the amount of tokens in circulation and
     distributed to each coin holder wallet. For example if loss pool held 10 
@@ -93,12 +120,17 @@ contract PricingCoin {
     Should return true if loss pool is completely distributed
     */
     function distributeLossPool() internal returns(bool){
-        
+
     }
     
     //Refund each users stake
     function refundStake() internal returns(bool) {
-        
+        // for(uint i=0; i<addresses.length; i++){
+        //     address a = voters[addresses[i]];
+        //     a.transfer(voters[addresses[i]].stake);
+        //     voters[addresses[i]].stake = 0;
+        // }
+        return true;
     }
     
 }
