d799791f57ee41e75a42ee60bf885e196e051a9f
KahnDigital1 <67976527+KahnDigital1@users.noreply.github.com>
2021-07-23 01:03:22 +0100

    front end change
    Change without landing page

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { SocialIcon } from 'react-social-icons';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1> Pricing Protocol is coming soon!  </h1>
+        <p>
+        Read our whitepaper <a href="https://github.com/alangindi/pricingcoin/blob/main/_Informal%20PricingProtocol%20White%20Paper.pdf">here</a>.
+        <br />
+        <br /> 
+        Stay up to date with us on Twitter, Github, and Discord.
+        <br />
+        <br />
+          <SocialIcon className="social" url="https://twitter.com/PricingProtocol" />
+          <SocialIcon className="social" url="https://github.com/alangindi/pricingcoin" />
+          <SocialIcon className="social" url="https://discord.gg/dSVWkcqCxS" />
+        </p> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
